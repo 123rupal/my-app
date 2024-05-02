abf3212cb0522c4da8207f570ac5a86a63934432
Rupal Patle <rupal08patle@gmail.com>
2024-05-02 16:02:39 +0530

    Dark mode throughout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import './App.css';
-import About from './components/About';
+//import About from './components/About';
 import Navbar from './components/Navbar';
-//import TextForm from './components/TextForm';
+import TextForm from './components/TextForm';
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor="#343a40";
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor="white";
+    }
+  }
+
   return (
     <>
-      <Navbar title="Textify" aboutText="About us"/>
+      <Navbar title="Textify" aboutText="About us" mode={mode} toggleMode={toggleMode}/>
       
       {//for default props: <Navbar/>
       }
-      {/*<div className="container my-3"> <TextForm heading="Enter text to be analyzed"/> </div>*/}
-      <About/>
+      <div className="container my-3"> <TextForm heading="Enter text to be analyzed" mode={mode}/> </div>
+      {/*<About/>*/}
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,13 +5,13 @@ export default function TextForm(props) {
     const [text,setText]=useState("");
   return (
     <>
-    <div>
+    <div className='container' style={{color:props.mode==='dark'?'white':'black'}}>
         <h1 className="heading">{props.heading}</h1>
         <div className="mb-3">
         <textarea className="form-control" value={text} id="myBox" rows="8" onChange={(event)=>{
             //console.log("On change.");
             setText(event.target.value);
-        }}>
+        }} style={{backgroundColor:props.mode==='dark'?'#343a40':'white', color:props.mode==='dark'?'white':'black'}}>
         </textarea>
     </div>
 
@@ -40,12 +40,12 @@ export default function TextForm(props) {
         }}>Replace a with e</button>
 
     </div>
-    <div className="container my-3"> 
+    <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}> 
         <h1>Your text summary</h1>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter text in the textbox to preview here."}</p>
     </div>
     </>
   );
